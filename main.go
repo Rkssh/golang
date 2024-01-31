5c3a7305e3d44a73eb4fa03e1c56d81ca7762c7d
Deep Singh <87003998+dipu-devops@users.noreply.github.com>
2024-01-31 15:02:53 +0530

    Update main.go

--- main.go
+++ main.go
@@ -4,30 +4,14 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	html := `<html><body style='background-color: blue; display: flex; justify-content: center; align-items: center; height: 100vh;'><h1 style='color: white;'>It is working!</h1></body></html>`
-	fmt.Fprintf(w, html)
-}
-
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		html := `<html><body style='background-color: blue; display: flex; justify-content: center; align-items: center; height: 100vh;'><h1 style='color: white;'>It is working!</h1></body></html>`
+		fmt.Fprintf(w, html)
+	})
 
-	http.HandleFunc("/", handler)
-
-	logger := log.New(os.Stdout, "devops-as-a-service: ", log.LstdFlags|log.Lshortfile)
-
-	server := &http.Server{
-		Addr:    ":" + port,
-		Handler: http.DefaultServeMux,
-		ErrorLog: logger,
-	}
-
-	fmt.Printf("Server listening on port %s...\n", port)
-	logger.Fatal(server.ListenAndServe())
+	fmt.Println("Server listening on port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
